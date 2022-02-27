aef32a8c77775484b2a1093b160266fd1e9e393f
Nandini <nandini@Nandinis-MacBook-Air.local>
2022-02-27 22:10:09 +0530

    Newfolder

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -4,7 +4,7 @@
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+        .constant('ApiBasePath', "https://www.sona-nyc.com")
         .directive('foundItems', FoundItems);
 
     function FoundItems() {
